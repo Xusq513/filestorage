eee85f7cece46634a68d5a8c794979eeafa0e2f9
xushaoqiang <xushaoqiang@ysstech.com>
2018-06-11 23:02:46 +0800

    加入了FastDfs的客户端上传

--- src/main/java/com/refutrue/filestorage/FilestorageApplication.java
+++ src/main/java/com/refutrue/filestorage/FilestorageApplication.java
@@ -1,9 +1,12 @@
 package com.refutrue.filestorage;
 
+import com.github.tobato.fastdfs.FdfsClientConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(FdfsClientConfig.class)
 @SpringBootApplication
 @MapperScan("com.refutrue.filestorage.mapper")
 public class FilestorageApplication {
